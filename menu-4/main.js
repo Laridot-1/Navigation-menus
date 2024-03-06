242a7c96391ceba0e6ae44a9b857de4440b11785
Benjamin <benjaminiyke090@gmail.com>
2024-03-06 18:00:42 +0100

    Pushed major changes

--- menu-4/main.js
+++ menu-4/main.js
@@ -4,8 +4,28 @@ const cont = darkMode.querySelector("div")
 const track = darkMode.querySelector(".track")
 const mode = darkMode.querySelector(".text")
 
+const links = document.querySelectorAll(".link")
+
+const toggleNav = document.querySelector(".toggle-nav")
+const nav = document.querySelector("nav")
+
+toggleNav.addEventListener("click", () => {
+  nav.classList.toggle("show")
+})
+
 cont.addEventListener("click", () => {
   body.classList.toggle("darkmode")
   if (body.classList.contains("darkmode")) mode.innerHTML = "Light Mode"
   else mode.innerHTML = "Dark Mode"
+})
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(a => {
+      if (a.classList.contains("active")) {
+        a.classList.remove("active")
+      }
+    })
+    link.classList.add("active")
+  })
 })
